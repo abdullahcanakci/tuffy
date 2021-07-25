5406555a26bb22274cf3f19272dda34c3b982eda
Abdullah Çanakçı <abdullahcanakci.0@gmail.com>
2021-07-25 15:55:25 +0300

    rename env

--- middlewares/UploadMiddleware.js
+++ middlewares/UploadMiddleware.js
@@ -7,8 +7,8 @@ const AWS_S3_BUCKET_NAME = process.env.AWS_S3_BUCKET_NAME;
 const s3 = new AWS.S3();
 
 AWS.config.update({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID_PER,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY_PER,
 });
 
 const UploadMiddleware = multer({
